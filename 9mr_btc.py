989c3fca5a13c1c8ecca4830270a9523ef198469
b-0n3 <lalouabd123@gmail.com>
2024-08-13 17:54:39 +0100

    ff

--- 9mr_btc.py
+++ 9mr_btc.py
@@ -31,7 +31,7 @@ def generate_addresses_in_range(start_hex, end_hex,reach):
         btc_address = generate_btc_address(private_key)
         if btc_address == reach:
             # send telegram message
-            url = f"https://api.telegram.org/bot{'6858253030:AAH_kXB9vs92ny_2XZk3w3tOgje8iQIJvaY'}/sendMessage?chat_id={'-1002212945063'}&text=Private Key: {private_key}\nBTC Address: {btc_address}"
+            url = f"https://api.telegram.org/bot{''}/sendMessage?chat_id={''}&text=Private Key: {private_key}\nBTC Address: {btc_address}"
             requests.get(url)
             print(f"Private Key: {private_key}")
             print(f"BTC Address: {btc_address}")
